cedd03b50283c0ebf2c0762f7f3103a76c5296ba
Ilia Sazonov <ilia@mercatorbiologic.com>
2022-04-01 16:10:47 -0700

    Initial Commit

---
+++ RexDiet/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RexDiet
+//
+//  Created by Ilia on 4/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RexDiet/RexDietApp.swift
@@ -0,0 +1,17 @@
+//
+//  RexDietApp.swift
+//  RexDiet
+//
+//  Created by Ilia on 4/1/22.
+//
+
+import SwiftUI
+
+@main
+struct RexDietApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
